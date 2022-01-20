d7ad3867d17256b13ab4d5c3d971d1a3721be8a2
Pamela Rod <akahime2014@gmail.com>
2022-01-20 16:08:45 -0300

    upload

---
+++ HelloWorld/app.py
@@ -0,0 +1,28 @@
+print("Hello World!")
+a = "Elvis Presley"
+b = 31
+c = 1.7
+#entre os sinais de atribuição precisa ter espaço.
+"""
+Multi-linhas entre aspas é comentário.
+Treinando comentários. Mesmo que a cor não fique cinza, o console não vai processar.
+Para concatenar, usa-se vírgula e dá espaço depois da virgula.
+No fim de cada linha de código é necessário pular de linha. Não é preciso um ; como no JS.
+"""
+print("A variável a é", a)
+print(type(a))
+print("A variável b é", b)
+print(type(b))
+print("A variável c é", c)
+print(type(c))
+
+#CASTING: Serve para forçar a variável ser de um tipo específico
+#EX:
+x = str(3) #será forçosamente uma variável de string '3'
+y = int(3) #será forçosamente uma variável inteira 3
+z = float(3) #será forçosamente uma variável float 3.0
+print(x,y,z)
+
+
+
+
