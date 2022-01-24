940507f033682cac0f07231df14cc0363d0f63e1
Pamela Rod <akahime2014@gmail.com>
2022-01-24 12:20:30 -0300

    desafiolbkg

---
+++ liparakg.py
@@ -0,0 +1,7 @@
+#Desafio de cálculo de conversão de Libras para quilos
+
+lb = input('Qual seu peso em Libras?    ')
+kg = int(lb) / 2.2046
+print('Seu peso em Quilos é ', kg)
+
+# Fórmula do Mosh  lb * 0.45  (160 lb que dá 72 kg)
